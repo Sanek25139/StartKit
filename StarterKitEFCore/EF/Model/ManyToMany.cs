4491480d73867c502a7f96f063881ee64d3028c0
Sanek25139 <106582676+Sanek25139@users.noreply.github.com>
2025-04-22 22:28:20 +0700

    StartKitV2.1.0

---
+++ StarterKitEFCore/EF/Model/ManyToMany.cs
@@ -0,0 +1,11 @@
+﻿namespace StarterKit.EF.Model
+{
+    public class ManyToMany<T1,T2> where T1 : BaseEntity where T2 : BaseEntity
+    {
+        public T1? Entity1 { get; set; }
+        public ulong Entity1Id { get; set; }
+
+        public T2? Entity2 { get; set; }
+        public ulong Entity2Id { get; set; }
+    }
+}
--- StarterKitEFCore/obj/Debug/net9.0/StarterKit.AssemblyInfo.cs
+++ StarterKitEFCore/obj/Debug/net9.0/StarterKit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StarterKit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56a44dbe9f3548069c9e01c2b6794dcfcd40b6c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24258b8aa4ee890e4b665cc8cdac8a1b91a71993")]
 [assembly: System.Reflection.AssemblyProductAttribute("StarterKit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StarterKit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
